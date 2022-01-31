cfa2d1e1cefc07237bd803144cb7f18e1a62a42b
yh2420 <yushanhu2020@163.com>
2022-01-31 12:20:58 +0000

    Addition and equality work!

--- polynomials/polynomials.py
+++ polynomials/polynomials.py
@@ -1,5 +1,4 @@
-from calendar import c
-from this import d
+from numbers import Number
 
 
 class Polynomial:
@@ -25,3 +24,32 @@ class Polynomial:
                 for d, c in enumerate(coefs[2:], start=2) if c] # 当c=!0时
         
         return " + ".join(reversed(terms)) or "0"
+
+    def __repr__(self):
+        return type(self).__name__ + "(" + repr(self.coefficients) + ")"
+
+    def __eq__(self, other):
+        
+        return self.coefficients == other.coefficients
+
+    def __add__(self, other):
+
+        if isinstance(other, Polynomial): # other 是Polynomial时
+
+            common = min(self.degree(), other.degree()) + 1 # 两者相加both contribute 的 term 个数
+            coefs = tuple(a + b for a, b in zip(self.coefficients, 
+                                                other.coefficients))  
+            # zip(list1,list2) list1，2中的元素一一对应地相加，ith+ith作为zip输出的ith，以len()小的相加对象为准
+            coefs += self.coefficients[common:] + other.coefficients[common:] 
+            # [common:]从common（含）到结束的所有index；tuple相加是在tuple原来基础上在后面append；self和other哪一个degree更高这里就加上哪个的coefs
+            return Polynomial(coefs)
+
+        elif isinstance(other, Number):
+            return Polynomial((self.coefficients[0] + other,) # tuple(,) length-1 tuple 有逗号
+                            + self.coefficients[1:])
+        
+        else:
+            return NotImplemented
+
+    def __radd__(self, other): # other + self
+        return self + other
--- tests/test_polynomials.py
+++ tests/test_polynomials.py
@@ -1,7 +1,31 @@
 from polynomials import Polynomial
+import pytest
 
 def test_print():
 
     p = Polynomial((2, 1, 0, 3))
 
-    assert str(p) == "3x^3 + x + 2"
+    assert str(p) == "3x^3 + x + 2"  # 和print()类似，对但是这个输出是string，有‘’
+
+def test_equality():
+    assert Polynomial((0,1)) == Polynomial((0,1))
+
+'''
+@pytest.mark.parametrize(
+    "a, b, sum"
+    (((0,), (0,1), (0,1)),  # 每行前两个括号代表的polynomial之和等于最后一个
+    ((2,0,3), (1,2), (3,2,3)),
+    (4,2), (10,2,4), (14,4,4)))
+    )
+'''
+
+def test_add(a, b, sum):
+    assert Polynomial(a) + Polynomial(b) == Polynomial(sum)
+
+def test_add_scalar():
+    assert Polynomial((2,1) + 3) == Polynomial((5,1))
+
+def test_add_unknown():
+    with pytest.raises(TypeError):  # 出来TypeError结果才算True
+        Polynomial((1,)) + "frog"
+
